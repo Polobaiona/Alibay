0efa08687eac8f20bc67d493e13ec10780af403c
gene <pocciaeugenio@gmail.com>
2019-05-06 14:35:06 -0400

    put code in Login and Signup elements

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -1,23 +1,61 @@
 import React, { Component } from "react"; // 1
 import { connect } from "react-redux";
 
-
 class UnconnectedLogin extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            username: '',
-            password: ''
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      password: ""
+    };
+  }
+
+  handleUsernameChange = event => {
+    this.setState({ username: event.target.value });
+  };
+  handlePasswordChange = event => {
+    this.setState({ password: event.target.value });
+  };
+
+  handleSubmit = evt => {
+    evt.preventDefault();
+
+    let data = new FormData();
+    data.append("username", this.state.username);
+    data.append("password", this.state.password);
+    fetch("http://localhost:4000/login", {
+      method: "POST",
+      body: data
+    })
+      .then(x => {
+        return x.text();
+      })
+      .then(responseBody => {
+        let body = JSON.parse(responseBody);
+
+        if (!body.success) {
+          alert("login failed");
+          return;
         }
-    }
+        this.props.dispatch({ type: "login-success" });
+      });
+  };
 
-    handleUsernameChange()
-    handlePasswordChange()
-    handleSubmit()
-
-    render = () => {}
-
+  render = () => {
+    return (
+      <div>
+        <h3>Login</h3>
+        <form onSubmit={this.handleSubmit}>
+          Username:
+          <input type="text" onChange={this.handleUsernameChange} />
+          Password:
+          <input type="text" onChange={this.handlePasswordChange} />
+          <input type="submit" />
+        </form>
+      </div>
+    );
+  };
 }
 
-let Login = connect()(UnconnectedLogin)
-export default Login
+let Login = connect()(UnconnectedLogin);
+export default Login;
