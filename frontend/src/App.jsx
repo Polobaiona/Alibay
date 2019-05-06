97b4d92a72d48ce8b3876f18fe405ffdeedd4111
brandon.tam29@gmail.com <Brandon Tam>
2019-05-06 15:14:45 -0400

    added some files and revamped the App a little

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,32 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import Signup from "./Signup.jsx";
-import Login from "./Login.jsx";
+import React, { Component } from "react"
+import { connect } from "react-redux"
+import Signup from "./Signup.jsx"
+import Login from "./Login.jsx"
 
 class UnconnectedApp extends Component {
   render = () => {
-    if (!this.props.lgin) {
-      return (
-        <div>
+    if (this.props.lgin) {
+      return(<div>
           <h1>Welcome to Alibay</h1>
-          <Signup /> <br />
+          <Signup /> 
           <Login />
         </div>
-      );
-    } else {
-      return <div>Alibay site!!!!</div>;
+      )
     }
-  };
+    return(<div> 
+        <div>Alibay site!!!!</div>
+        <SearchBar />
+        <Account />
+        <Categories />
+        <Items />
+      </div>
+      )
+  }
 }
 
 let mapStateToProps = state => {
-  return { lgin: state.loggedIn };
-};
+  return { lgin: state.loggedIn }
+}
 
-let App = connect(mapStateToProps)(UnconnectedApp);
-export default App;
+let App = connect(mapStateToProps)(UnconnectedApp)
+export default App
