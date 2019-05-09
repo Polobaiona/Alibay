a6f02dd189da58e19798356e64b3924baa917930
brandon.tam29@gmail.com <Brandon Tam>
2019-05-09 14:19:51 -0400

    hi

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,13 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import Signup from "./Signup.jsx";
-import Login from "./Login.jsx";
-import Categories from "./Categories.jsx";
-import ItemCollection from "./ItemCollection.jsx";
-import Account from "./Account.jsx";
-import SearchBar from "./SearchBar.jsx";
+import React, { Component } from "react"
+import { connect } from "react-redux"
+import Signup from "./Signup.jsx"
+import Login from "./Login.jsx"
+import Categories from "./Categories.jsx"
+import ItemCollection from "./ItemCollection.jsx"
+import Account from "./Account.jsx"
+import SearchBar from "./SearchBar.jsx"
 import ItemDetails from "./ItemDetails.jsx"
-import { BrowserRouter, Route, Link } from "react-router-dom";
+import { BrowserRouter, Route, Link } from "react-router-dom"
 
 class UnconnectedApp extends Component {
 
@@ -15,17 +15,19 @@ class UnconnectedApp extends Component {
     let itemArray = []
     fetch("http://localhost:4000/items")
     .then(x => {
-      return x.text();
+      return x.text()
     })
     .then(responseBody => {
-      console.log("rendering all items");
-      let body = JSON.parse(responseBody);
-      let items = body.itemDetails;
-      console.log(itemArray);
+      console.log("rendering all items")
+      console.log(responseBody)
+      let body = JSON.parse(responseBody)
+      console.log(body)
+      let items = body.itemDetails
+      console.log(itemArray)
   
       items.map(item => {
-        itemArray.push(item);
-      });
+        itemArray.push(item)
+      })
     })
     this.props.dispatch({
       type: 'fetchItems', 
@@ -34,10 +36,16 @@ class UnconnectedApp extends Component {
   }
   renderAccount = () => <Account />
 
-  renderItemDetails = () => <ItemDetails />
+  renderItemDetails = routerData => {
+    let path = routerData.match.params.itemId
+    return(<ItemDetails path={path}/>)
+  }
+
+  
+  
 
   renderRoot = () => {
-    console.log("app props", this.props);
+    console.log("app props", this.props)
     return (
       <div>
         <div className="flex">
@@ -51,8 +59,8 @@ class UnconnectedApp extends Component {
           <ItemCollection />
         </div>
       </div>
-    );
-  };
+    )
+  }
 
   render = () => {
     if (!this.props.loggedIn) {
@@ -62,7 +70,7 @@ class UnconnectedApp extends Component {
           <Signup />
           <Login />
         </div>
-      );
+      )
     } else {
       return (
         <div>
@@ -74,17 +82,17 @@ class UnconnectedApp extends Component {
             </div>
           </BrowserRouter>
         </div>
-      );
+      )
     }
-  };
+  }
 }
 
 let mapStateToProps = state => {
   return { 
     loggedIn: state.loggedIn,
     allItems: state.allItems
-  };
-};
+  }
+}
 
-let App = connect(mapStateToProps)(UnconnectedApp);
-export default App;
+let App = connect(mapStateToProps)(UnconnectedApp)
+export default App
